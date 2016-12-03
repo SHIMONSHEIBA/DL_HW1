593150dc3b15952a8524115cf380537285bfa3da
reutapel <reutapel88@gmail.com>
2016-12-03 18:11:44 +0200

    Update test_function.lua

--- test_function.lua
+++ test_function.lua
@@ -1,13 +1,15 @@
+local mnist = require 'mnist';
+
 function TestModel()
 	model = torch.load('ourModel.dat')
 	
-	testData = testSet.data:float();
-	testLabels = testSet.label:add(1);
+	testData = mnist.testdataset().data:float();
+	testLabels = mnist.testdataset().label:add(1);
 	
 	local confusion = optim.ConfusionMatrix(torch.range(0,9):totable())
-    local lossAcc = 0
-    local numBatches = 0
-    local batchSize = 16
+	local lossAcc = 0
+	local numBatches = 0
+	local batchSize = 16
 	
     for i = 1, testData:size(1) - batchSize, batchSize do
         numBatches = numBatches + 1
