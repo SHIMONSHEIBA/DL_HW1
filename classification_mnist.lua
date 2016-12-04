9b21c7ea7d963f01447d15aede18af9c1d7410f5
reutapel <reutapel88@gmail.com>
2016-12-04 17:38:12 +0200

    Update classification_mnist.lua

--- classification_mnist.lua
+++ classification_mnist.lua
@@ -100,7 +100,7 @@ function forwardNet(data, labels, train, e)
                 return err, dE_dw
             end
 		--optimization function to use
-            optim.adagrad(feval, w, optimState)
+            optim.adagrad(feval, w)
         end
     end
     
